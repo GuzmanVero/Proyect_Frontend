64b07ac7aa572a9f52d09b42fde001205185bfcf
GuzmanVero <marcyguz19@gmail.com>
2023-09-23 00:47:16 -0600

    creacion y finalizacion de iniciarsesion.js y detalles en html y css

---
+++ js/iniciarsesion.js
@@ -0,0 +1,7 @@
+function iniciosesion(){
+    var nameuser = document.getElementById("nameuser").value;
+    var password = document.getElementById("password").value;
+    
+    var mensaje = "Nombre de usuario: " + nameuser + "\nInicio de sesion exitoso.";
+            alert(mensaje);
+}
